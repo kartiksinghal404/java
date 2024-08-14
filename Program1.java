6dfc4cb578d77f2562606a4a80617c5df240628f
kartiksinghal404 <119917591+kartiksinghal404@users.noreply.github.com>
2024-08-14 20:38:15 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,18 @@
+class A{
+public static void main(String aa[])
+{
+int x=345;
+System.out.println("ABCD");
+System.out.println("23");
+System.out.println(123.56);
+double y=23.67;
+System.out.println('x');
+System.out.println(12*10+5);
+System.out.println("x");
+System.out.println(x);
+System.out.println(y);
+System.out.println("value of x is:"+x);
+}
+}
+
+
